b62663dc32f4165980dccfe0549d2232ed45770b
Gunarto Septiawan <gunartosept@gmail.com>
2021-09-25 15:46:18 +0700

    tugas_2_javascript
    Tugas Niomic

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var string = "Hallo"
+var angka = 12345
+var oke = "Hallo Dunia"
+oke = null;
+var matematika;
+
+var panjang = false;
+var lebar = 200;
+
+if (lebar<220)
+panjang = true;
+else
+panjang = false;
+
+console.log(string);
+console.log(angka);
+console.log(string);
+console.log(matematika);
+console.log(panjang);
